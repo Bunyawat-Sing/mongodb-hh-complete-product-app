fac64261620c29913e0ac1faf9a724e1c3d3ab5e
Bunyawat Singkiporn <pupahero@gmail.com>
2024-11-15 15:46:27 +0700

    feat: add crud api using mongodb

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,15 +1,80 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
+import { ObjectId } from "mongodb";
 
 const productRouter = Router();
 
-productRouter.get("/", (req, res) => {});
+//read all product
+productRouter.get("/", async (req, res) => {
+  try {
+    //use products collection from practice-mango database
+    const productsCollection = db.collection("products");
+    //find all data from collection
+    const products = await productsCollection.find({}).toArray();
+    //return response
+    return res.json({ data: products });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
-productRouter.get("/:id", (req, res) => {});
+//create new product
+productRouter.post("/", async (req, res) => {
+  try {
+    //use products collection from practice-mango database
+    const productsCollection = db.collection("products");
+    //declare valiable to recieve request
+    //insert data to collection
+    const productsData = { ...req.body };
+    await productsCollection.insertOne(productsData);
+    //return response
+    return res.json({ message: "Product has been created successfully" });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
-productRouter.post("/", (req, res) => {});
+//update product
+productRouter.put("/:productId", async (req, res) => {
+  try {
+    //use products collection from practice-mango database
+    const productsCollection = db.collection("products");
+    //declare valiable to recieve productId from client using ObjectId
+    const productId = new ObjectId(req.params.productId);
+    //declare valiable to recieve request body from client
+    const newProductData = { ...req.body };
+    //update product
+    await productsCollection.updateOne(
+      { _id: productId },
+      { $set: newProductData }
+    );
+    //return response
+    return res.json({
+      message: "Product has been updated successfully",
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
-productRouter.put("/:id", (req, res) => {});
-
-productRouter.delete("/:id", (req, res) => {});
+//delete product
+productRouter.delete("/:productId", async (req, res) => {
+  try {
+    //use products collection from practice-mango database
+    const productsCollection = db.collection("products");
+    //declare valiable to recieve productId from client using ObjectId
+    //delete product by productId
+    const productId = new ObjectId(req.params.productId);
+    await productsCollection.deleteOne({
+      _id: productId,
+    });
+    //return response
+    return res.json({
+      message: "Product has been deleted successfully",
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 export default productRouter;
